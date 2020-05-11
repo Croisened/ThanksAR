663c4bb42c94973cf0bc4e41cd59a63fe7e65368
Nate Ryan <croisened@gmail.com>
2020-05-11 17:58:10 -0500

    Added the load by script option

---
+++ script.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'MurderHornet',
+            location: {
+                lat: 41.153363503827364,
+                lng: -96.0140934749382,
+            }, 
+            path: './assets/magnemite/scene.gltf'
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+        let path = place.path;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', path);
+        model.setAttribute('rotation', '0 90 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '100 100 100');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
